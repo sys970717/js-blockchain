ae3684009dd700f01dea90acc28db21352d826c5
sys970717 <sys970717@naver.com>
2021-06-10 01:41:58 +0900

    트랜잭션 추가 및 새로운 블록 생성하기.

--- src/clone/index.js
+++ src/clone/index.js
@@ -7,8 +7,11 @@ bitcoin.createNewBlock(1111, 'aaaaaaa', '1a1a1a1a1a1a');
 //새로은 트랜잭션 생성 - (총금액, 보내는이, 받는이)
 bitcoin.createNewTransaction(100, 'HELLo', 'GIILE');
 
-// bitcoin.createNewBlock(2222, 'bbbbbbb', '2b2b2b2b2b2b');
-// bitcoin.createNewBlock(3333, 'ccccccc', '3c3c3c3c3c3c');
+// 추가 트랜잭션 생성.
+bitcoin.createNewTransaction(50, 'HELLo', 'GIILE');
+bitcoin.createNewTransaction(20, 'HELLo', 'GIILE');
+
+bitcoin.createNewBlock(2222, bitcoin.getPreviousBlockHash(), '1b1b1b1b1b1b');
 
 console.log(bitcoin);
 console.log(bitcoin.printChain());
